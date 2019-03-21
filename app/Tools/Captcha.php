374ae407a37fdf6d88114ad8c78a530557ea80e9
zhangbaomin455 <1617352802@qq.com>
2019-03-21 16:30:32 +0800

    shop

--- app/Tools/Captcha.php
+++ app/Tools/Captcha.php
@@ -15,7 +15,7 @@ class Captcha {
     //构造方法初始化
     public function __construct() {
         //echo dirname(__FILE__);die;
-        $this->font = public_path().'/fonts/Elephant.ttf';//注意字体路径要写对，否则显示不了图片
+        $this->font = public_path().'./fonts/Elephant.ttf';//注意字体路径要写对，否则显示不了图片
     }
     //生成随机码
     private function createCode() {
